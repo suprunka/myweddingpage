d0b5a852318daa8fa3db67b15de4a4e5e765b98e
Patrycja Suprun <patrycja@snapp.dk>
2021-05-16 22:40:42 +0200

    cors web

--- src/services/formService.js
+++ src/services/formService.js
@@ -1,7 +1,12 @@
 import axios from "axios"
 export function addMessage(request)
 {
-    axios.post('https://szalone-wesele-api.herokuapp.com/Messages', request)
+    axios.post('https://szalone-wesele-api.herokuapp.com/Messages', request, { headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Access-Control-Allow-Headers":
+          "Origin, Content-Type, X-Auth-Token",
+  }})
     .then(function (response) {
         console.log(response);
       })
