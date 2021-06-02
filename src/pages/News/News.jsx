f68c19f992ed0eb88d281424549a6a5888594494
Patrycja Suprun <patrycja@snapp.dk>
2021-06-02 09:49:31 +0200

    change title

--- src/pages/News/News.jsx
+++ src/pages/News/News.jsx
@@ -11,11 +11,12 @@ const News = ()=>
  oraz odwiezie Was bezpiecznie do domu po zakończeniu przyjęcia.
 </p>
 <p>
-    Wegani wegetranie- alergie-
+   Jesli posiadasz alergie pokarmowe, badz jestes na diecie- napisz do nas, przygotujemy dla Ciebie odpowiednie dania!
 </p>
 <p>
 Polecane noclegi w okolicy:
 <ul>
+<li>- Noclegi w budynku przy sali, jest 8 wolnych pokoi(150zl/noc)</li>
 <li>- Agroturystyka</li>
 </ul>
 
